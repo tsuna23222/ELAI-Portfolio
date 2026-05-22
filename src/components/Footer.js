5e6a918b246e49c16d3b785d89f03f0c7a6170fa
tsuna23222 <richardcasinilloph@gmail.com>
2026-05-22 11:28:39 +0800

    fooothger changes adding games page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,6 +24,7 @@ function Footer() {
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/projects">Projects</Link>
+          <Link to="/games">Games</Link>
           <Link to="/contact">Contact</Link>
         </div>
 
